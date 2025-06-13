d381dcd59176d1b8ed91c7843644021d5b9f664b
putri-alie <shafiraputri.aliesta@gmail.com>
2025-06-13 10:23:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("hellooo ini shafiraaa!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "mari bermain dengan saya")
+
